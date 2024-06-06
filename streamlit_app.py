5949aaa599df389f9f7d00f5cb5bf49f9031de0d
DMAO-MQ <33108357+psyche911@users.noreply.github.com>
2024-06-07 01:04:28 +1000

    Update streamlit_app.py
    Use Our fruit_chosen Variable in the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,11 @@ if ingredients_list:
 
     # FOR LOOP Block works to convert the LIST to a STRING
     ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     #st.write(ingredients_string) --only for testing
