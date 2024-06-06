b61482783800b1c4d8932117144e9e1076e41b51
DMAO-MQ <33108357+psyche911@users.noreply.github.com>
2024-06-07 01:07:17 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,13 @@ ingredients_list = st.multiselect(
 
 # Cleaning Up Empty Brackets
 if ingredients_list:
-    #st.write(ingredients_list) --only for testing
-    #st.text(ingredients_list) --only for testing
-
-    # FOR LOOP Block works to convert the LIST to a STRING
-    ingredients_string = ''
-    
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        ingredients_string = ''
+        
+        for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chosen + ' '
+                st.subheader(fruit_chosen + ' Nutrition Information')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     #st.write(ingredients_string) --only for testing
 
